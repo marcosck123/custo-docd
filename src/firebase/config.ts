08964d7f5cbc4668c6d4e8e4bb7de52280f2349e
Marcos Eduardo <marcoseduardock@gmail.com>
2026-03-08 17:23:40 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5026630963-e2aee",
+  "appId": "1:1006023799648:web:a2766e32bcb1d03110a405",
+  "apiKey": "AIzaSyA5op46Peep71ZUfZaqGTs4mujMMKOCx1I",
+  "authDomain": "studio-5026630963-e2aee.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1006023799648"
+};
